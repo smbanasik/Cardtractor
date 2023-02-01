bdd0c52a106e087df7df753b995101d8047f3482
smbanasik <smbanasik@gmail.com>
2023-01-31 18:45:11 -0600

    Flexible Particles
    Direction not limited to up

--- Angel.gd
+++ Angel.gd
@@ -5,9 +5,8 @@ extends Area2D
 # var a = 2
 # var b = "text"
 var screen_size
-export var angelSpeed = 400
-var angelRadians = 0
-var angel
+export var angelSpeed = 200
+export var angelMaxDistance = 10
 export var projectileSpeed = 500
 export var projectileDamage = 1
 var projectileRadians = PI
@@ -27,6 +26,11 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO
 	
+	# Angel moves to ThinkPoint
+	if self.position != $ThinkPoint.position:
+		
+		pass
+	
 	# Prevent angel from moving off screen by ensuring the value remains
 	# between the min (0), and max (screen size)
 	position.x = clamp(position.x, 0, screen_size.x)
@@ -38,5 +42,21 @@ func _on_Angel_body_entered(body):
 
 # AI stuff
 func _on_ThinkTime_timeout():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
+	# Angel picks a direction
+	var angelRadians = rng.randf_range(0, 2*PI)
+	# Angel picks a distance
+	rng.randomize()
+	var angelDistance = rng.randf_range(0, angelMaxDistance)
+	
+	# Angel moves a normalized vector * distance in the direction
+	var angelVector = Vector2.UP.rotated(angelRadians) * angelDistance
+	
+	# Current position + vector = thinkPoint, clamped from 0 : screensize
+	$ThinkPoint.position = self.position + angelVector
+	$ThinkPoint.position.x = clamp($ThinkPoint.position.x, 0, screen_size.x)
+	$ThinkPoint.position.y = clamp($ThinkPoint.position.y, 0, screen_size.y)
 	
 	pass
