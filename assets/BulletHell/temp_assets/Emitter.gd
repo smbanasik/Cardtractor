e11f645d695e4acf8cf4d9ae6d6ab88da3f9a563
smbanasik <smbanasik@gmail.com>
2023-02-06 03:00:03 -0600

    Added comments

--- assets/BulletHell/temp_assets/Emitter.gd
+++ assets/BulletHell/temp_assets/Emitter.gd
@@ -5,6 +5,19 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+# Emitters - Will serve as an abstraction between enemies and projectile positioning & such
+# - Main will still actually create the projectiles, but this way, it's easier to produce interesting projectile patterns
+# - Handle the position, direction, damage, acceleration, starting velocity, and arrays of projectiles.
+# - These do not handle projectile curvature or movement once fired. That will be left up to projectile types
+# - Wants: These may or may not be possible, but ideally this is how it works
+# - I want emitters to be attachable to enemies, so a generic angel may use different types of emitters
+# - I want the emitter to recieve when the angel fires and act on that.
+# - So we make an emitter scene, we flesh it out, we attach it to an angel. Then what?
+# - I don't think we can just make different emitter scenes and attach it to one angel, so the emitter should be robust.
+# - One type of emitter, capable of manipulating every aspect of every projectile through different modes/functions.
+# - It might get messy, but I can't think of another way right now.
+# - This would provide the most flexibility as enemies could use multiple attacks if need be.
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
